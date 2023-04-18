0aa82583d3cc2d2e48d6731574361534c11d9ad3
Jonathan Cenfo <jjimenezz@ucenfote.ac.cr>
2023-04-18 10:58:20 -0600

    add endpoint to return phrase by Id

--- src/main/java/com/tfcards/tf_cards_rest/tf_cards_rest/controllers/DemoResourceController.java
+++ src/main/java/com/tfcards/tf_cards_rest/tf_cards_rest/controllers/DemoResourceController.java
@@ -10,6 +10,7 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -66,6 +67,15 @@ public class DemoResourceController {
         Map<String, Object> res = new HashMap<>();
         res.put("object", this.demoService.getAll());
         res.put("msg", "Phrases were fetched sucessfully!");
+        return res;
+    }
+
+    @GetMapping({ "/{id}" })
+    public Map<String, Object> getById(@PathVariable("id") Long pId) {
+        Map<String, Object> res = new HashMap<>();
+        var phraseFound = this.demoService.get(pId);
+        res.put("object", phraseFound);
+        res.put("msg", String.format("Phrase with id %s was fetched sucessfully!", pId));
         return res;
     }
 
