e2b2f214514466b7ccf09555385eeb5fbc03eed4
Jonathan GG <jimenezzjti99@gmail.com>
2023-05-30 00:15:23 -0600

    unit test for update v1 phrase

--- src/test/java/com/tfcards/tf_cards_rest/tf_cards_rest/controllers/DemoResourceControllerTest.java
+++ src/test/java/com/tfcards/tf_cards_rest/tf_cards_rest/controllers/DemoResourceControllerTest.java
@@ -22,9 +22,12 @@ import org.springframework.test.web.servlet.request.RequestPostProcessor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.util.StandardCharset;
+import com.tfcards.tf_cards_rest.configuration.SecurityConfigTest;
+import com.tfcards.tf_cards_rest.tf_cards_rest.commands.PhraseBaseCommand;
 import com.tfcards.tf_cards_rest.tf_cards_rest.configuration.SecurityConfiguration;
 import com.tfcards.tf_cards_rest.tf_cards_rest.converters.PhraseBaseToPhraseCommand;
 import com.tfcards.tf_cards_rest.tf_cards_rest.converters.PhraseCommandToPhraseBase;
+import com.tfcards.tf_cards_rest.tf_cards_rest.domain.enums.EPhraseType;
 import com.tfcards.tf_cards_rest.tf_cards_rest.services.IDemoService;
 
 import static org.mockito.BDDMockito.*;
@@ -32,89 +35,120 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
 @WebMvcTest(DemoResourceController.class)
+@Import(SecurityConfigTest.class)
 class DemoResourceControllerTest {
 
-    private static final String MOCK_USERNAME = "admin";
-    private static final String MOCK_USER_PASS = "dummyadmin";
+        private static final String MOCK_USERNAME = "admin";
+        private static final String MOCK_USER_PASS = "dummyadmin";
 
-    @Autowired
-    MockMvc mockMvc;
+        @Autowired
+        MockMvc mockMvc;
 
-    @Autowired
-    ObjectMapper objWrapper;
+        @Autowired
+        ObjectMapper objWrapper;
 
-    @Autowired
-    MessageSource msgSrc;
+        @Autowired
+        MessageSource msgSrc;
 
-    @MockBean
-    IDemoService demoService;
+        @MockBean
+        IDemoService demoServiceMock;
 
-    // @MockBean
-    // MessageSource msgSourceMock;
+        // @MockBean
+        // MessageSource msgSourceMock;
 
-    PhraseCommandToPhraseBase phraseConverter;
+        PhraseCommandToPhraseBase phraseConverter;
 
-    PhraseBaseToPhraseCommand phraseCmdConverter;
+        PhraseBaseToPhraseCommand phraseCmdConverter;
 
-    @Test
-    @WithMockUser(username = "admin", password = "dummyadmin", roles = { "CLIENT", "ADMIN" })
-    void testGetPhrase() throws Exception {
-        var user = "admin";
-        var pass = "dummyadmin";
-        var userName = "Kratos";
-        //
-        var phraseExpected = this.msgSrc.getMessage("demo.phrase", new String[] { userName }, "",
-                new Locale("en"));
-        // given(this.msgSourceMock.getMessage(eq("demo.phrase"), any(String[].class),
-        // anyString(), any(Locale.class)))
-        // .willReturn(phraseExpected);
-        //
-        mockMvc.perform(
-                get("/v1/demo/hello")
-                        .with(SecurityMockMvcRequestPostProcessors.httpBasic(user, pass))
-                        .accept(MediaType.APPLICATION_JSON)
-                        .param("name", userName)
-                        .header("Accept-Language", "en"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.msg", instanceOf(String.class)))
-                .andExpect(jsonPath("$.msg", is(phraseExpected)));
-    }
+        @Test
+        @WithMockUser(username = "admin", password = "dummyadmin", roles = { "CLIENT", "ADMIN" })
+        void testGetPhrase() throws Exception {
+                var user = "admin";
+                var pass = "dummyadmin";
+                var userName = "Kratos";
+                //
+                var phraseExpected = this.msgSrc.getMessage("demo.phrase", new String[] { userName }, "",
+                                new Locale("en"));
+                // given(this.msgSourceMock.getMessage(eq("demo.phrase"), any(String[].class),
+                // anyString(), any(Locale.class)))
+                // .willReturn(phraseExpected);
+                //
+                mockMvc.perform(
+                                get("/v1/demo/hello")
+                                                .with(SecurityMockMvcRequestPostProcessors.httpBasic(user, pass))
+                                                .accept(MediaType.APPLICATION_JSON)
+                                                .param("name", userName)
+                                                .header("Accept-Language", "en"))
+                                .andExpect(status().isOk())
+                                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(jsonPath("$.msg", instanceOf(String.class)))
+                                .andExpect(jsonPath("$.msg", is(phraseExpected)));
+        }
 
-    @Test
-    @WithMockUser(username = MOCK_USERNAME, password = MOCK_USER_PASS, roles = { "CLIENT" })
-    void testGetPhraseInEs() throws Exception {
-        var userName = "Patricio";
-        var phraseExpected = this.msgSrc.getMessage("demo.phrase", new String[] { userName }, "", new Locale("es"));
-        //
-        mockMvc.perform(
-                get("/v1/demo/hello")
-                        .with(SecurityMockMvcRequestPostProcessors.httpBasic(MOCK_USERNAME, MOCK_USERNAME))
-                        .accept(MediaType.APPLICATION_JSON)
-                        .param("name", userName)
-                        .header("Accept-Language", "es"))
-                .andExpectAll(
-                        status().isOk(),
-                        content().contentType(MediaType.APPLICATION_JSON),
-                        jsonPath("$.msg", instanceOf(String.class)),
-                        jsonPath("$.msg", is(phraseExpected)));
-    }
+        @Test
+        @WithMockUser(username = MOCK_USERNAME, password = MOCK_USER_PASS, roles = { "CLIENT" })
+        void testGetPhraseInEs() throws Exception {
+                var userName = "Patricio";
+                var phraseExpected = this.msgSrc.getMessage("demo.phrase", new String[] { userName }, "",
+                                new Locale("es"));
+                //
+                mockMvc.perform(
+                                get("/v1/demo/hello")
+                                                .with(SecurityMockMvcRequestPostProcessors.httpBasic(MOCK_USERNAME,
+                                                                MOCK_USERNAME))
+                                                .accept(MediaType.APPLICATION_JSON)
+                                                .param("name", userName)
+                                                .header("Accept-Language", "es"))
+                                .andExpectAll(
+                                                status().isOk(),
+                                                content().contentType(MediaType.APPLICATION_JSON),
+                                                jsonPath("$.msg", instanceOf(String.class)),
+                                                jsonPath("$.msg", is(phraseExpected)));
+        }
 
-    @Test
-    @WithMockUser(username = MOCK_USERNAME, password = MOCK_USER_PASS, roles = { "CLIENT" })
-    void testPostPhrase() throws Exception {
-        var userName = "Patricio";
-        Map<String, String> newPhrase = new Hashtable<>(
-                Map.of("id", "0", "phrase", "Heeeeeeeeeeeey DUDE?", "phraseType", "5"));
-        mockMvc.perform(
-                post("/v1/demo")
-                        .with(SecurityMockMvcRequestPostProcessors.httpBasic(MOCK_USERNAME, MOCK_USERNAME))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objWrapper.writeValueAsString(newPhrase)))
-                .andExpect(status().isCreated());
-    }
+        @Test
+        @WithMockUser(username = MOCK_USERNAME, password = MOCK_USER_PASS, roles = { "CLIENT" })
+        void testPostPhrase() throws Exception {
+                Map<String, String> newPhrase = new Hashtable<>(
+                                Map.of("id", "0", "phrase", "Heeeeeeeeeeeey DUDE?", "phraseType", "5"));
+                var mockPhrase = new PhraseBaseCommand(0L, newPhrase.get("phrase"));
+                mockPhrase.setPhraseType(EPhraseType.GREET);
+                //
+                given(this.demoServiceMock.create(any(PhraseBaseCommand.class))).willReturn(mockPhrase);
+                //
+                mockMvc.perform(
+                                post("/v1/demo")
+                                                .with(SecurityMockMvcRequestPostProcessors.httpBasic(MOCK_USERNAME,
+                                                                MOCK_USER_PASS))
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(objWrapper.writeValueAsString(newPhrase)))
+                                .andExpect(status().isCreated());
+        }
+
+        @Test
+        @WithMockUser(username = MOCK_USERNAME, password = MOCK_USER_PASS, roles = { "CLIENT" })
+        public void testUpdatePhrase() throws Exception {
+                var mockPhrase = new PhraseBaseCommand(2L, "Hi, how is it going?");
+                mockPhrase.setPhraseType(EPhraseType.GREET);
+                //
+                given(this.demoServiceMock.update(any(PhraseBaseCommand.class))).willReturn(mockPhrase);
+                //
+                mockMvc.perform(
+                                put("/v1/demo/{phraseId}", mockPhrase.getId())
+                                                .with(SecurityMockMvcRequestPostProcessors.httpBasic(MOCK_USERNAME,
+                                                                MOCK_USER_PASS))
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(this.objWrapper.writeValueAsString(mockPhrase)))
+                                .andExpect(status().isOk())
+                                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(jsonPath("$.msg", instanceOf(String.class)))
+                                .andExpect(jsonPath("$.obj", instanceOf(Map.class)));
+                //
+                verify(this.demoServiceMock).update(any(PhraseBaseCommand.class));
+        }
 }
