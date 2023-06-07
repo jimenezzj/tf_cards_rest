c0dae50975fa558a4b40afdd5be62c01ad10c241
JonathanGG <jimenezzjti99@gmail.com>
2023-06-07 14:09:43 -0600

    JPA unit test oneither save phrase and save phrase with errors

--- src/test/java/com/tfcards/tf_cards_rest/tf_cards_rest/repositories/DemoRepoTest.java
+++ src/test/java/com/tfcards/tf_cards_rest/tf_cards_rest/repositories/DemoRepoTest.java
@@ -1,6 +1,7 @@
 package com.tfcards.tf_cards_rest.tf_cards_rest.repositories;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.test.context.ActiveProfiles;
 
 import com.tfcards.tf_cards_rest.tf_cards_rest.domain.PhraseBase;
 import com.tfcards.tf_cards_rest.tf_cards_rest.domain.enums.EPhraseType;
+
+import jakarta.validation.ConstraintViolationException;
 
 @DataJpaTest
 @ActiveProfiles("dev")
@@ -20,11 +23,22 @@ public class DemoRepoTest {
     @Test
     public void testSavePhrase() {
         PhraseBase savedPhrase = this.demoRepo.save(
-                PhraseBase.builder().phrase("What' up, bro?").phraseType(EPhraseType.GREET)
+                PhraseBase.builder()
+                        .phrase("What' up, bro?")
+                        .phraseType(EPhraseType.GREET)
                         .build());
+        this.demoRepo.flush();
         assertNotNull(savedPhrase, "Returned phrase should not be null");
         assertNotNull(savedPhrase.getId());
         assertNotNull(savedPhrase.getPhrase());
+    }
+
+    @Test
+    void testSaveWithMissingFields() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            this.demoRepo.save(PhraseBase.builder().build());
+            this.demoRepo.flush();
+        });
     }
 
 }
